c453306170922f3492f0f4707582c89b96d91f27
Waswa Rodgers <waswam1994@gmail.com>
2020-07-23 21:30:54 +0300

    Created a function and defined constants to receive form elements

---
+++ javascripts/app.js
@@ -0,0 +1,11 @@
+(function(){
+    const reviewerName = document.querySelector("reviewer_name");
+    const reviewerImage = document.querySelector("reviewer_image");
+    const reviewerTestimonial = document.querySelector("reviewer_testimonial");
+    const testimonialButton = document.querySelectorAll(".btn");
+
+    console.log(reviewerName);
+    console.log(reviewerTestimonial);
+    console.log(reviewerImage);
+    console.log("Here i am!")
+})()
